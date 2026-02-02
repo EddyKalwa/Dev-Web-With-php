290e4fd7313d1aa029a275b2781299b8af3457c7
KALUNGA KALWA EDDY <eddykalwa00@gmail.com>
2026-02-02 10:42:03 +0200

    Seance 4 adding promotion data

--- ind.php
+++ ind.php
@@ -1,6 +1,25 @@
-<?php $header = "L'shi Promo"; ?>
-
+<?php 
+    $header = "Welcome Home l'shi Promo";
+?>
 <?php require 'portion/header.php' ?>
-<h1 class="text-3xl font-bold text-center mt-10 text-white">Welcome to Our Website</h1>
-<?php require 'portion/footer.php' ?>
 
+<h1 class="text-2xl font-bold text-white">Profiter de  nos meilleures offres</h1>
+
+<?php require 'models/promotions-data.php' ?>
+
+<?php foreach($promotions as $magasin => $produits): ?>
+
+<div>
+    <h1 class='text-white'>Magasin : <?php echo $magasin; ?></h1>
+    <?php foreach($produits as $produit => $details): ?>
+    <p class='text-white'>Nom de l'article : <?php echo $produit; ?></p>
+    <p class='text-white'>Prix initial : <?php echo $details['prix-initial']; ?><?php echo $details['device']; ?></p>
+    <p class='text-white'>Prix promo: <?php echo $details['prix-promo']; ?><?php echo $details['device']; ?></p>
+    <p class='text-white'>reductionPourcentage: <?php echo round((($details['prix-initial'] - $details['prix-promo']) / $details['prix-initial']) * 100); ?>%</p>
+    <br>
+    <?php endforeach; ?>
+</div>
+
+<?php endforeach; ?>
+
+<?php require 'portion/footer.php' ?>
---
+++ models/promotions-data.php
@@ -0,0 +1,23 @@
+<?php
+
+$promotions = [
+    'Top-Market' => [
+        'iPhone 16' => [
+            'prix-initial' => 350,
+            'prix-promo' => 280,
+            'device' => '$',
+        ],
+        'prix Twiga' => [
+            'prix-initial' => 53000,
+            'prix-promo' => 42000,
+            'device' => 'FC',
+        ],
+    ],
+    'kin Marche' => [
+        'Lait Nido' => [
+            'prix-initial' => 37500,
+            'prix-promo' => 22000,
+            'device' => 'FC',
+        ],
+    ],
+];
